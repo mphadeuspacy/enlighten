2e77c4ddd99ab719d4decab5dc59f47570f3bd2f
martim00 <martim00@c6754fae-200f-ba55-fcd9-d8b08427dd90>
2010-08-06 16:35:16 +0000

    Renomeando classes e iniciando parse. Teste que está falhando deve passar ao terminar o parser.

--- branches/unit-fit/SimpleTest.cpp
+++ branches/unit-fit/SimpleTest.cpp
@@ -3,51 +3,107 @@ using namespace igloo;
 
 #include "FluentInterfaceHelper.h"
 
+typedef std::vector<std::string> Rows;
 
-class QualquerCoisa
+class ExampleFixture
 {
 public:
    void ParaOsDados(const std::string &file)
    {
+      //parse
       
-      executeTest();
+      execute();
    }
 
-   virtual void executeTest()= 0;
+   virtual void execute()= 0;
+
+   template <class T>
+   T getCellAs(const std::string label, int row)
+   {
+      T valueAsInt;
+      std::stringstream ss(columns[label][row].c_str());
+      ss >> valueAsInt;
+      return valueAsInt;      
+   }
+
+   int ExampleCount() const
+   {
+      return exampleCount;
+   }
+
 protected:
-   std::map<std::string, std::vector<std::string> > dataMap;
+   int exampleCount;
+   std::map<std::string, Rows> columns;
 };
 
-class CalculeIMC : public QualquerCoisa
+#define DECLARE_COLUMN(name, klass)  \
+   klass &name(const std::string &_##name)   \
+   {  \
+      columns[_##name]= Rows();  \
+      return *this;  \
+   }  
+
+
+
+
+class CalculeIMC : public ExampleFixture
 {
 public:
    DECLARE_SUGAR(ParaPessoa, CalculeIMC);
    DECLARE_SUGAR(Com, CalculeIMC);
    DECLARE_SUGAR(E, CalculeIMC);
+   DECLARE_COLUMN(Altura, CalculeIMC);
+   DECLARE_COLUMN(Peso, CalculeIMC);
+   DECLARE_COLUMN(EsperaSe, CalculeIMC);   
 
-   void Peso(const std::string &strPeso)
-   {
-      dataMap[strPeso]= std::vector<std::string> >();
+   void execute()
+   {   
+      for (int i= 0; i < exampleCount; i++)
+      {
+         double altura= this->getCellAs<double>("altura", i);
+         double peso= this->getCellAs<double>("peso", i);
+
+      }
+
+      
    }
-
-   void executeTest()
-   {
-
-   }
-
 };
 
-Context(Some)
+struct SimpleFixture : public ExampleFixture
 {
-   Spec(ShouldHaveAnEmptyBoard)
+   DECLARE_COLUMN(SomeColumn, SimpleFixture);
+
+   void execute()
+   {
+   
+   }
+};
+
+Context(SimpleExamples)
+{
+
+   Spec(ShouldReadExampleFile)
+   {
+      SimpleFixture fixture;
+      fixture.SomeColumn("some")
+         .ParaOsDados("|some|\n|50|");
+
+      Assert::That(fixture.ExampleCount(), Is().EqualTo(1));
+
+            
+   }
+
+   Spec(ShouldFailOnError)
    {
       CalculeIMC()
          .ParaPessoa().Com()
          .Peso("peso")
          .E()
          .Altura("altura")
-         .EsperaSe("result")
-      .ParaOsDados("dados.txt");
+         .EsperaSe("imc")
+      //.ParaOsDados("dados.txt");
+      .ParaOsDados(" \
+         |altura|peso|imc?|  \
+         |50|50|100|");
    }
-   
 };
