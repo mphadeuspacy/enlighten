e96fd8cf5b712becb0253e2c8ac12188fa9136de
diego.fernandoribeiro <diego.fernandoribeiro@gmail.com>
2010-08-10 18:02:08 +0000

    tudo no .h

--- SimpleTest.cpp
+++ SimpleTest.cpp
@@ -11,23 +11,25 @@ public:
    DECLARE_SUGAR(ParaPessoa, CalculeIMC);
    DECLARE_SUGAR(Com, CalculeIMC);
    DECLARE_SUGAR(E, CalculeIMC);
-   //DECLARE_COLUMN(Altura, CalculeIMC);
-   //DECLARE_COLUMN(Peso, CalculeIMC);
-   //DECLARE_COLUMN(EsperaSe, CalculeIMC);   
+   DECLARE_COLUMN(Altura, CalculeIMC);
+   DECLARE_COLUMN(Peso, CalculeIMC);
+   DECLARE_COLUMN(EsperaSe, CalculeIMC);   
 
    void execute()
    {   
-      for (int i= 0; i < exampleCount; i++)
+      for (int i= 0; i < ExampleCount(); i++)
       {
          double altura= this->getCellAs<double>("altura", i);
          double peso= this->getCellAs<double>("peso", i);
+			double imc= altura / (peso*peso);
+         
       }      
    }
 };
 
 struct SimpleFixture : public Fixture
 {
-   //DECLARE_COLUMN(SomeColumn, SimpleFixture);
+   DECLARE_COLUMN(SomeColumn, SimpleFixture);
 
    void execute()
    {
@@ -41,46 +43,38 @@ Context(SimpleExamples)
 	Spec(Tokenizer)
 	{
 		TableParser parser;		
-		Fixture::Table table= parser.LoadTable("|altura|peso|imc|\n|1.74|73|24.11|\n|1.63|62|23.33|\n|1.52|51|22.07|");
+		Table table= parser.LoadTable("|altura|peso|imc|\n|1.74|73|24.11|\n|1.63|62|23.33|\n|1.52|51|22.07|");
 		Assert::That(table.size(), Is().EqualTo(3));	
       Assert::That(numberOfFields(table), Is().EqualTo(3));	
 	}
 
-   size_t numberOfFields(const Fixture::Table &table) const
+   size_t numberOfFields(const Table &table) const
    {
       if (table.empty()) return 0;
       else return table.begin()->size();
    }
 
 	Spec(ParserFromFile)
-	{
-		TableParser parser;
-		Fixture::Table table= parser.LoadTableFromFile("teste.txt");
+	{		
+		Table table= TableParser().LoadTableFromFile("teste.txt");
 		Assert::That(table.size(), Is().EqualTo(3));	
 	}
 
-   /*Spec(ShouldReadExampleFile)
+   Spec(ShouldReadExampleFile)
    {
       SimpleFixture fixture;
-      fixture.SomeColumn("some")
-         .ParaOsDados("|some|\n|50|");
+		//fixture.GetTable()= TableParser().LoadTable("|some|\n|50|");	
+      Assert::That(fixture.ExampleCount(), Is().EqualTo(1));            
+   }
 
-      Assert::That(fixture.ExampleCount(), Is().EqualTo(1));
-
-            
-   }*/
-
-   /*Spec(ShouldFailOnError)
+   Spec(ShouldFailOnError)
    {
       CalculeIMC()
          .ParaPessoa().Com()
          .Peso("peso")
          .E()
          .Altura("altura")
-         .EsperaSe("imc")
-      //.ParaOsDados("dados.txt");
-      .ParaOsDados(" \
-         |altura|peso|imc?|  \
-         |50|50|100|");
-   }*/
+         .EsperaSe("imc")      
+         .ParaOsDados();
+   }
 };
