2d7dd856bbe2daa7f4a85ef8d7045fee2b919b61
martim00 <martim00@c6754fae-200f-ba55-fcd9-d8b08427dd90>
2010-08-11 15:18:03 +0000

    Adding support for more than one table per file

--- Fixture.h
+++ Fixture.h
@@ -4,45 +4,79 @@
 #include <map>
 #include <string>
 #include <vector>
+#include "Table.h"
 #include "TableParser.h"
 #include "FixtureGlossary.h"
+
+
 
 
 class Fixture
 {
 public:	
+
+   ~Fixture()
+   {
+      TableIterator it= FirstTable();
+      for (; it != LastTable(); it++)
+         delete (*it).second;
+   }
 	
 	void LoadingData(const std::string &data)
-   {  		 
-		table= TableParser().LoadTable(data);
-		execute();
+   {
+		tables= TableParser().LoadTables(data);    
+   	execute();
    }
 
 	void LoadingDataFromFile(const std::string &fileName)
 	{  		 
-		table= TableParser().LoadTableFromFile(fileName);
+		//table= TableParser().LoadTableFromFile(fileName);
+
+      tables= TableParser().LoadTablesFromFile(fileName);
 	   execute();
 	}
 
    virtual void execute()= 0;
 
    template <class T>
-   T getCellAs(const std::string field, int row)
+   T getCellAs(const std::string tableName, const std::string field, int row)
    {
       T valueAsInt;
-      std::stringstream ss(table[row][field].c_str());
+      std::stringstream ss(GetTable(tableName)->GetRows()[row][field].c_str());
       ss >> valueAsInt;
       return valueAsInt;
    }
 
-   int ExampleCount() const
+   /*int ExampleCount() const
    {
-      return table.size();
+      return tables.GetRows().size();
+   }*/
+
+   Table *GetTable(const std::string &tableName)
+   {
+      if (tables.find(tableName) == tables.end())
+         throw std::runtime_error("cant find a table with name " + tableName);
+      return tables[tableName];
+   }  
+   
+   
+
+   typedef std::map<std::string, Table*>::iterator TableIterator;
+   TableIterator FirstTable()
+   {
+      return tables.begin();
+   }
+
+   TableIterator LastTable()
+   {
+      return tables.end();
    }
 
 protected:
-   Table table;	
-   std::vector<std::string> fields;  	
+   std::map<std::string, Table*> tables;
+   //std::vector<Table*> tables;
+   //Table table;
+   std::vector<std::string> fields;
 };
 
 #endif //INCLUDED_FIXTURE_H
